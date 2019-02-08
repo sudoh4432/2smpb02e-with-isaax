74b042161fd5eb3fafd25a6f4eb9392190b1d229
sudoh4432 <sudoh.yoshi@gmail.com>
2019-02-08 20:20:49 +0900

    Update script.js

--- static/script.js
+++ static/script.js
@@ -26,18 +26,18 @@ Highcharts.chart('chart', {
       }
     },
     title: {
-      text: 'Pressure'
+      text: 'Templerature'
     },
     xAxis: {
       type: 'datetime'
     },
     yAxis: {
       title: {
-        text: 'hPa' 
+        text: 'C' 
       }
     },
     series: [{
-      name: 'Pressure',
+      name: 'Templerature',
       data: (function () {
         let data = [],
             time = (new Date()).getTime(),
